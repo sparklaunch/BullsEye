4ca73bf93499eebfc8fb219200064ac99162d6f6
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-28 12:26:01 +0900

    Initial Commit

---
+++ Shared/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/03/28.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
