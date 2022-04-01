cd3c7348714e801675d9b10e3f7ff9691c00623d
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-01 10:09:37 +0900

    Add the colors for the buttons.

---
+++ Shared/Views/RoundedViews.swift
@@ -0,0 +1,57 @@
+//
+//  RoundedViews.swift
+//  BullsEye (iOS)
+//
+//  Created by Jinwook Kim on 2022/04/01.
+//
+
+import SwiftUI
+
+struct RoundedImageViewStroked: View {
+    let systemName: String
+    var body: some View {
+        Image(systemName: systemName)
+            .font(.title)
+            .foregroundColor(Color("TextColor"))
+            .padding()
+            .overlay(
+                Circle()
+                    .strokeBorder(Color("ButtonStrokeColor"), lineWidth: 2)
+            )
+    }
+}
+
+struct RoundedImageViewFilled: View {
+    let systemName: String
+    var body: some View {
+        Image(systemName: systemName)
+            .font(.title)
+            .foregroundColor(Color("ButtonFillTextColor"))
+            .padding()
+            .background(
+                Circle()
+                    .fill(Color("ButtonFillBackgroundColor"))
+            )
+    }
+}
+
+struct PreviewView: View {
+    var body: some View {
+        Group {
+            RoundedImageViewStroked(systemName: "arrow.counterclockwise")
+            RoundedImageViewFilled(systemName: "list.dash")
+        }
+    }
+}
+
+struct RoundedViews_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            PreviewView()
+            PreviewView()
+                .preferredColorScheme(.dark)
+        }
+        .padding()
+        .previewLayout(.sizeThatFits)
+    }
+}
